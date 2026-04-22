9f8970fb10c4f1dd32777af2571fe3ab59c568f3
jang-mac <jang-mac@jang-macui-MacBookPro.local>
2026-04-22 12:55:50 +0900

    chore: upgrade dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import prettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,12 +10,11 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     ignores: ["**/node_modules/**", ".next/**", "dist/**", "coverage/**"],
     rules: {
       "no-unused-vars": "warn",
     },
   },
-  prettier,
 ];
